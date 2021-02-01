5a7dd7078e63ebe537a95a2bef3cea4092127ae2
xtream9029 <qocn9029@naver.com>
2021-02-01 19:04:21 +0900

    upload files

--- test.cpp
+++ test.cpp
@@ -1,87 +1,77 @@
-#include <string>
-#include <vector>
 #include <iostream>
+#include <vector>
 #include <algorithm>
 #include <queue>
 #include <stack>
-#include <stdio.h>
-#include <sstream>
+#include <string>
 #include <string.h>
+#include <sstream>
 #include <map>
 #include <set>
+#include <cmath>
 using namespace std;
 
-struct Data {
-    int former_x;
-    int former_y;
-    int current_x;
-    int current_y;
-    int cost;
+struct pa {
+	int x;
+	int y;
+	int state;
 };
 
-int D[25][25];
-bool check[25][25];
+int n, m;
+int Map[1000][1000];
+int check[1000][1000][2];//단순 2D check로는 풀 수가 없음
 int dx[4] = { -1,1,0,0 };
 int dy[4] = { 0,0,-1,1 };
-int Min = 987654321;
 
-int bfs(vector<vector<int>> board) {
-    int Size = board.size();
-    queue<Data> q;
-    check[0][0] = true;
+int bfs() {
+	queue<pa> q;
+	q.push(pa{ 0,0,0 });
+	check[0][0][0] = true;
 
-    if (board[0][1] == 0) {
-        q.push(Data{ 0,0,0,1,100 });
-        check[0][1] = true;
-    }
-    if (board[1][0] == 0) {
-        q.push(Data{ 0,0,1,0,100 });
-        check[1][0] = true;
-    }
+	while (q.size()) {
+		pa cur = q.front();
+		q.pop();
 
-    while (q.size()) {
-        Data cur = q.front();
-        q.pop();
+		if (cur.x == n - 1 && cur.y == m - 1) {
+			//cur.state값이 0인지 1인지는 알수가 없음
+			return check[cur.x][cur.y][cur.state]+1;
+		}
 
-        if (cur.current_x == Size - 1 && cur.current_y == Size - 1) {
-            Min = min(Min, cur.cost);
-            D[Size - 1][Size - 1] = Min;
-        }
+		for (int i = 0; i < 4; i++) {
+			int nx = cur.x + dx[i];
+			int ny = cur.y + dy[i];
 
-        for (int i = 0; i < 4; i++) {
-            int nx = cur.current_x + dx[i];
-            int ny = cur.current_y + dy[i];
+			if (nx < 0 || nx >= n) continue;
+			if (ny < 0 || ny >= m) continue;
 
-            if (nx < 0 || nx >= Size) continue;
-            if (ny < 0 || ny >= Size) continue;
+			if (Map[nx][ny] == 0) {
+				check[nx][ny][cur.state] = check[cur.x][cur.y][cur.state] + 1;
+				q.push(pa{ nx,ny,cur.state});
+			}
 
-            if (board[nx][ny] == 0) {
-                int new_cost = cur.cost + 100;
-
-                if (abs(nx - cur.former_x) == 1 && abs(ny - cur.former_y) == 1)
-                    new_cost += 500;
-
-                if (!check[nx][ny]) {
-                    check[nx][ny] = true;
-                    q.push(Data{ cur.current_x,cur.current_y,nx,ny,new_cost });
-                    D[nx][ny] = new_cost;
-                }
-                else {
-                    if (cur.cost <= D[nx][ny]) {
-                        q.push(Data{ cur.current_x,cur.current_y,nx,ny, new_cost });
-                        D[nx][ny] = new_cost;
-                    }
-                }
-            }
-        }//for
-    }//while
-    return D[Size - 1][Size - 1];
+			else if (Map[nx][ny] == 1) {//벽인 경우
+				if (check[nx][ny][cur.state]==0) {
+					check[nx][ny][cur.state+1] = check[cur.x][cur.y][cur.state]+1;
+					q.push(pa{ nx,ny,cur.state + 1 });
+				}
+			}
+		}//for
+	}//while
+	return -1;
 }
 
-int solution(vector<vector<int>> board) {
-    int answer = 0;
-    memset(check, false, sizeof(check));
-    memset(D, 0, sizeof(D));
-    answer = bfs(board);
-    return answer;
+
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		for (int j = 0; j < (int)s.length(); j++) {
+			Map[i][j] = s[j] - '0';
+		}
+	}
+	memset(check, false, sizeof(check));
+	cout << bfs();
+	return 0;
 }
