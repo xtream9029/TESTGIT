9a47733b0984334869b7b62c9458ad5e244a871b
xtream9029 <qocn9029@naver.com>
2021-02-02 09:54:24 +0900

    upload files

--- test.cpp
+++ test.cpp
@@ -11,67 +11,55 @@
 #include <cmath>
 using namespace std;
 
-struct pa {
-	int x;
-	int y;
-	int state;
-};
+#define MAX 987654321
+int Min = MAX;//가장 짧은걸 구해야 하므로
 
-int n, m;
-int Map[1000][1000];
-int check[1000][1000][2];//단순 2D check로는 풀 수가 없음
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,-1,1 };
+int solution(string s) {
+    int answer = 0;
 
-int bfs() {
-	queue<pa> q;
-	q.push(pa{ 0,0,0 });
-	check[0][0][0] = true;
+    for (int i = 1; i <= s.length(); i++) {
+        string tmp = "";
+        vector<string> v;
 
-	while (q.size()) {
-		pa cur = q.front();
-		q.pop();
+        //문자열을 자르는 과정
+        for (int k = 0; k < s.length(); k++) {
+            tmp += s[k];
 
-		if (cur.x == n - 1 && cur.y == m - 1) {
-			//cur.state값이 0인지 1인지는 알수가 없음
-			return check[cur.x][cur.y][cur.state]+1;
-		}
+            if (tmp.length() == i) {
+                v.push_back(tmp);
+                tmp = "";
+            }
+        }
+        v.push_back(tmp);
 
-		for (int i = 0; i < 4; i++) {
-			int nx = cur.x + dx[i];
-			int ny = cur.y + dy[i];
+        string S = "";
+        int cnt = 1;
+        for (int k = 0; k < v.size() - 1; k++) {
+            if (v[k] == v[k + 1]) {
+                cnt++;
+            }
+            else {
+                if (cnt == 1) {
+                    S += v[k];
+                }
+                else if (cnt > 1) {
+                    S += to_string(cnt) + v[k];
+                }
+                cnt = 1;
+            }
+        }
+        S += v[v.size() - 1];
 
-			if (nx < 0 || nx >= n) continue;
-			if (ny < 0 || ny >= m) continue;
-
-			if (Map[nx][ny] == 0) {
-				check[nx][ny][cur.state] = check[cur.x][cur.y][cur.state] + 1;
-				q.push(pa{ nx,ny,cur.state});
-			}
-
-			else if (Map[nx][ny] == 1) {//벽인 경우
-				if (check[nx][ny][cur.state]==0) {
-					check[nx][ny][cur.state+1] = check[cur.x][cur.y][cur.state]+1;
-					q.push(pa{ nx,ny,cur.state + 1 });
-				}
-			}
-		}//for
-	}//while
-	return -1;
+        if (Min > S.length()) Min = S.length();
+    }
+    answer = Min;
+    return answer;
 }
 
-
-
 int main() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		string s;
-		cin >> s;
-		for (int j = 0; j < (int)s.length(); j++) {
-			Map[i][j] = s[j] - '0';
-		}
-	}
-	memset(check, false, sizeof(check));
-	cout << bfs();
-	return 0;
+    string s;
+    cin >> s;
+
+    cout << solution(s);
+    return 0;
 }
