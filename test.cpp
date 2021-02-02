2b9537d3eb1f15ff2fbcd8419843c1ac100aae5d
xtream9029 <qocn9029@naver.com>
2021-02-02 19:02:22 +0900

    upload files

--- test.cpp
+++ test.cpp
@@ -11,55 +11,53 @@
 #include <cmath>
 using namespace std;
 
-#define MAX 987654321
-int Min = MAX;//가장 짧은걸 구해야 하므로
-
-int solution(string s) {
-    int answer = 0;
-
-    for (int i = 1; i <= s.length(); i++) {
-        string tmp = "";
-        vector<string> v;
-
-        //문자열을 자르는 과정
-        for (int k = 0; k < s.length(); k++) {
-            tmp += s[k];
-
-            if (tmp.length() == i) {
-                v.push_back(tmp);
-                tmp = "";
-            }
-        }
-        v.push_back(tmp);
-
-        string S = "";
-        int cnt = 1;
-        for (int k = 0; k < v.size() - 1; k++) {
-            if (v[k] == v[k + 1]) {
-                cnt++;
-            }
-            else {
-                if (cnt == 1) {
-                    S += v[k];
-                }
-                else if (cnt > 1) {
-                    S += to_string(cnt) + v[k];
-                }
-                cnt = 1;
-            }
-        }
-        S += v[v.size() - 1];
-
-        if (Min > S.length()) Min = S.length();
-    }
-    answer = Min;
-    return answer;
-}
+int primeNumber[4000001];
 
 int main() {
-    string s;
-    cin >> s;
 
-    cout << solution(s);
-    return 0;
+	int n;
+	cin >> n;
+	for (int i = 0; i <= 4000000; i++) {
+		primeNumber[i] = 1;
+	}
+
+	primeNumber[1] = 0;
+	for (int i = 2; i * i <= n; i++) {
+		for (int j = i * i; j <= n; j += i) {
+			if (primeNumber[j] == 0) continue;
+			primeNumber[j] = 0;
+		}
+	}
+
+	int cnt = 0;
+	vector<int> v;
+	for (int i = 2; i <= n; i++) {
+		if (primeNumber[i] == 1) v.push_back(i);
+	}
+	
+	for (int i = 1; i < (int)v.size(); i++) {
+		v[i] += v[i - 1];
+	}//for
+
+	int S;
+	int left = -1;
+	int right = 0;
+	while(left<=right){
+		if (right >= (int)v.size()) break;
+		
+		if (left == -1) S = v[right];
+		else S = v[right] - v[left];
+
+		if (S < n) right += 1;
+		else if (S == n) {
+			cnt++;
+			right += 1;
+		}
+		else {
+			left += 1;
+		}
+	}//while
+
+	std::cout << cnt;
+	return 0;
 }
