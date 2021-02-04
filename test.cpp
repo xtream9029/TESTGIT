4e3c12807584bfe7c1d432a722a18ff42382a524
xtream9029 <qocn9029@naver.com>
2021-02-04 09:40:28 +0900

    upload files

--- test.cpp
+++ test.cpp
@@ -11,53 +11,74 @@
 #include <cmath>
 using namespace std;
 
-int primeNumber[4000001];
+struct pa {
+    int x;
+    int y;
+};
+
+bool check(string s, vector<string> v) {
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == s) return false;
+    }
+    return true;
+}
+
+vector<int> solution(vector<string> gems){
+    vector<int> answer;
+    set<string> Set;
+
+    pa ANSWER;
+    for (int i = 0; i < gems.size(); i++) {
+        Set.insert(gems[i]);
+    }
+
+    int left = 0;
+    int right = 0;
+    int dif = gems.size();
+    int S;
+
+    //투포인터 알고리즘
+    while (left <= right){
+        if (right == gems.size()) break;
+
+        S = right - left+1;
+
+        if (S < Set.size()) {
+            right += 1;
+        }
+        else if (S >= Set.size()) {
+            set<string> A;
+            for (int i = left; i <= right; i++) {
+                A.insert(gems[i]);
+            }
+            if (Set == A){
+                if (S - 1 < dif) {
+                    dif = S - 1;
+                    ANSWER.x = left;
+                    ANSWER.y = right;
+                }
+                left += 1;
+            }
+            else right += 1;
+        }
+    }//while
+
+    answer.push_back(ANSWER.x + 1);
+    answer.push_back(ANSWER.y + 1);
+    return answer;
+}
 
 int main() {
+    int n;
+    cin >> n;
+    vector<string> v;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        v.push_back(s);
+    }
 
-	int n;
-	cin >> n;
-	for (int i = 0; i <= 4000000; i++) {
-		primeNumber[i] = 1;
-	}
-
-	primeNumber[1] = 0;
-	for (int i = 2; i * i <= n; i++) {
-		for (int j = i * i; j <= n; j += i) {
-			if (primeNumber[j] == 0) continue;
-			primeNumber[j] = 0;
-		}
-	}
-
-	int cnt = 0;
-	vector<int> v;
-	for (int i = 2; i <= n; i++) {
-		if (primeNumber[i] == 1) v.push_back(i);
-	}
-	
-	for (int i = 1; i < (int)v.size(); i++) {
-		v[i] += v[i - 1];
-	}//for
-
-	int S;
-	int left = -1;
-	int right = 0;
-	while(left<=right){
-		if (right >= (int)v.size()) break;
-		
-		if (left == -1) S = v[right];
-		else S = v[right] - v[left];
-
-		if (S < n) right += 1;
-		else if (S == n) {
-			cnt++;
-			right += 1;
-		}
-		else {
-			left += 1;
-		}
-	}//while
-
-	std::cout << cnt;
-	return 0;
+    vector<int> rt = solution(v);
+    cout << rt[0] << ' ' << rt[1];
+    return 0;
 }
